066bede5d96ac1877254484295ed9ca4861455fd
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-01-30 13:40:21 +0100

    Creada la clase PostPutDelProduct

--- app/src/main/java/com/network/AddProductActivity.java
+++ app/src/main/java/com/network/AddProductActivity.java
@@ -3,12 +3,31 @@ package com.network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddProductActivity extends AppCompatActivity {
+    TextInputEditText productName, productPrice, unitProduct;
+    Button buttonSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        productName = findViewById(R.id.product_name);
+        productPrice = findViewById(R.id.product_price);
+        unitProduct = findViewById(R.id.unit_product);
+        buttonSave = findViewById(R.id.button_save);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/network/PostPutDelProduct.java
@@ -0,0 +1,38 @@
+package com.network;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PostPutDelProduct {
+    @SerializedName("status")
+    String status;
+
+    @SerializedName("result")
+    String result;
+
+    @SerializedName("message")
+    String message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
